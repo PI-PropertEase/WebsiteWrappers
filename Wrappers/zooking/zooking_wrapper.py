56f72fe37a43c52ef73ac1439d3fcb47188885ea
Bárbara Galiza <barnobregali02@gmail.com>
2024-04-12 00:05:52 +0100

    import properties done

--- Wrappers/zooking/zooking_wrapper.py
+++ Wrappers/zooking/zooking_wrapper.py
@@ -1,11 +1,19 @@
 import requests
 from ..base_wrapper.api_wrapper import BaseAPIWrapper
+from ProjectUtils.MessagingService.queue_definitions import channel, EXCHANGE_NAME, WRAPPER_ZOOKING_ROUTING_KEY
 
 
 class ZookingAPIWrapper(BaseAPIWrapper):
     def __init__(self) -> None:
         super().__init__()
         self.url = "http://localhost:8000/"
+        self.queue = "zooking_queue"
+        zooking_queue = channel.queue_declare(queue=self.queue, durable=True)
+        channel.queue_bind(
+            queue=zooking_queue.method.queue,
+            exchange=EXCHANGE_NAME,
+            routing_key=WRAPPER_ZOOKING_ROUTING_KEY,
+        )
 
     def create_property(self, property):
         url = self.url + "properties"
@@ -23,3 +31,8 @@ class ZookingAPIWrapper(BaseAPIWrapper):
         print("Deleting property...")
         url = self.url + f"properties/{_id}"
         requests.delete(url=url)
+
+    def import_properties(self, user):
+        url = self.url + "properties?email=" + user.get("email")
+        print("Importing properties...")
+        return requests.get(url=url).json()
