fa83f361a3ac555cb58afd134951c62db73d8ae0
Migas77 <miguel.belchior@ua.pt>
2024-04-27 16:34:46 +0100

    PTSE-69 separate message processing between 2 files

--- Wrappers/zooking/zooking_reservation_handler.py
+++ Wrappers/zooking/zooking_reservation_handler.py
@@ -5,7 +5,6 @@ from ProjectUtils.MessagingService.schemas import (
     MessageType,
     to_json, from_json, Service,
 )
-from Wrappers.models import set_property_mapped_id
 from .zooking_wrapper import ZookingAPIWrapper
 
 
@@ -16,25 +15,6 @@ def handle_recv(channel, method, properties, body):
     print(message.__dict__)
     body = message.body
     match message.message_type:
-        case MessageType.PROPERTY_CREATE:
-            wrapper.create_property(body)
-        case MessageType.PROPERTY_UPDATE:
-            wrapper.update_property(body["internal_id"], body["update_parameters"])
-        case MessageType.PROPERTY_DELETE:
-            wrapper.delete_property(body)
-        case MessageType.PROPERTY_CREATE:
-            wrapper.create_property(body)
-        case MessageType.PROPERTY_IMPORT:
-            properties = wrapper.import_properties(body)
-            channel.basic_publish(exchange=EXCHANGE_NAME, routing_key=WRAPPER_TO_APP_ROUTING_KEY, body=to_json(
-                MessageFactory.create_import_properties_response_message(Service.ZOOKING, properties)
-            ))
-            reservations = wrapper.import_reservations(body)
-            channel.basic_publish(exchange=EXCHANGE_NAME, routing_key=WRAPPER_TO_CALENDAR_ROUTING_KEY, body=to_json(
-                MessageFactory.create_import_reservations_response_message(Service.ZOOKING, reservations)
-            ))
-        case MessageType.PROPERTY_IMPORT_DUPLICATE:
-            set_property_mapped_id(Service.ZOOKING, body["old_internal_id"], body["new_internal_id"])
         case MessageType.RESERVATION_IMPORT_OVERLAP:
             print("RESERVATION_IMPORT_OVERLAP: ", body)
             wrapper.delete_reservation(body["old_internal_id"])
