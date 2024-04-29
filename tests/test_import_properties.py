e2440d60aabeda73ceb5011a83ff2253967e5bb9
notjoao1 <joao.dourado1@ua.pt>
2024-04-29 13:48:38 +0100

    PTSE-79 tests: tested import properties message behavior (need to mock pika.BlockingConnection)

--- tests/test_import_properties.py
+++ tests/test_import_properties.py
@@ -1,7 +1,6 @@
 import pytest
 from pytest_mock import MockerFixture
 from unittest.mock import MagicMock, Mock
-from Wrappers.models import set_property_internal_id
 from Wrappers.regular_events_handler import handle_recv
 from Wrappers.zooking.zooking_wrapper import ZookingWrapper
 from Wrappers.zooking.converters.zooking_to_propertease import ZookingToPropertease
@@ -11,8 +10,6 @@ from ProjectUtils.MessagingService.queue_definitions import (
     WRAPPER_TO_APP_ROUTING_KEY,
 )
 from pydantic import BaseModel
-from requests import Response
-
 
 # required for the fake_user fixture
 class User(BaseModel):
@@ -21,10 +18,14 @@ class User(BaseModel):
 
 @pytest.fixture
 def fake_user():
-    return User(email="coolguy@gmail.com")
+    return User(email="cool_guy@gmail.com")
 
 
-def test_import_properties_zooking_empty_db(mocker: MockerFixture, fake_user: User):
+def test_import_properties_zooking_receive_message_return_properties(mocker: MockerFixture, fake_user: User):
+    """
+        Only testing that receiving the IMPORT_PROPERTIES message triggers a basic_publish
+        with the same data that was imported.
+    """
     # mock/setup test
     channel_mock = MagicMock()
     method_mock = MagicMock()
@@ -38,30 +39,30 @@ def test_import_properties_zooking_empty_db(mocker: MockerFixture, fake_user: Us
                 {"user_email":"cool_guy@gmail.com","name":"A mais fixe de todas 🪂🛸","address":"Morada João","curr_price":50.0,"description":"descrição não interessa a ninguém","number_of_guests":4,"square_meters":250,"bedrooms":{"bedroom_1":[{"number_beds":2,"bed_type":"king_bed"},{"number_beds":1,"bed_type":"single_bed"}]},"bathrooms":[{"name":"bathroom_groundfloor","bathroom_fixtures":["toilet","tub"]}],"amenities":["AC","wifi"],"additional_info":"Se cá aparecerem, recebem um rebuçado","id":12}
             ]
 
+    def fake_internal_id(*args, **kwargs):
+        return args[1] * 2      # 2 * property_external_id
+    
+    mocker.patch("Wrappers.zooking.converters.zooking_to_propertease.set_property_internal_id", side_effect=fake_internal_id)
+    
+    mock_requests_get = Mock()
     mock_response = Mock()
+    mock_requests_get.return_value = mock_response
     mock_response.json.return_value = user_properties_response
-    # mock requests.get to return predefined JSON
-    mocker.patch("requests.get", return_value=mock_response)
-
-    mocker.patch("Wrappers.models.set_property_internal_id", return_value=1)
+    mocker.patch("requests.get", mock_requests_get)
+    
+    mocker.patch("ProjectUtils.MessagingService.schemas.time", return_value=1)
 
     # act
     handle_recv(channel_mock, method_mock, properties_mock, message_body, wrapper)
-
-    properties_after_conversion = [ZookingToPropertease.convert_property(prop) for prop in user_properties_response]
 
     expected_body = to_json(MessageFactory.create_import_properties_response_message(
         service=wrapper.service_schema, properties=[ZookingToPropertease.convert_property(prop) for prop in user_properties_response]
     ))
 
-    # asserts
-    # 1. assert basic_publish called with - o tipo de mensagem esperado + o body esperado
+    mock_requests_get.assert_called_once_with(url=wrapper.url + "properties?email=cool_guy@gmail.com")
     channel_mock.basic_publish.assert_called_once_with(
         exchange=EXCHANGE_NAME,
         routing_key=WRAPPER_TO_APP_ROUTING_KEY,
         body=expected_body,
     )
-    # 2. assert que a mensagem foi acknowledged (basic_ack called i guess?)
     channel_mock.basic_ack.assert_called_once()
-    # 3. assert estado da DB quando acaba
-    assert False == True
