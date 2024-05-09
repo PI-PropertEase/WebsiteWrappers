e2f59eceeabec603fa1bace5965ae455bcd3f097
Diana <dianarrmiranda@gmail.com>
2024-05-09 11:59:28 +0100

    Separate each wrapper into its own container

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 -m Wrappers.zooking.zooking_regular_handler &
-python3 -m Wrappers.zooking.zooking_scheduled_handler &
-python3 -m Wrappers.earthstayin.earthstayin_regular_handler &
-python3 -m Wrappers.earthstayin.earthstayin_scheduled_handler &
-python3 -m Wrappers.clickandgo.clickandgo_regular_handler &
-python3 -m Wrappers.clickandgo.clickandgo_scheduled_handler &
-wait
