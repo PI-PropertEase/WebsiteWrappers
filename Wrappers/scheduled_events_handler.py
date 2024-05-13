7b1f6078fa0b41a0cd9735b626a85d550dc94716
notjoao1 <joao.dourado1@ua.pt>
2024-05-13 11:53:50 +0100

    PTSE-107 improved logging on scheduled_events_handler

--- Wrappers/scheduled_events_handler.py
+++ Wrappers/scheduled_events_handler.py
@@ -1,3 +1,5 @@
+import logging
+
 from ProjectUtils.MessagingService.queue_definitions import channel, EXCHANGE_NAME, WRAPPER_TO_APP_ROUTING_KEY, \
     WRAPPER_TO_CALENDAR_ROUTING_KEY
 from ProjectUtils.MessagingService.schemas import (
@@ -7,37 +9,40 @@ from ProjectUtils.MessagingService.schemas import (
 )
 from Wrappers.base_wrapper.wrapper import BaseWrapper
 
+LOGGER = logging.getLogger(__name__)
+
 
 def handle_recv(channel, method, properties, body, wrapper:BaseWrapper):
     delivery_tag = method.delivery_tag
-
+    LOGGER.info("%s: received message", wrapper.service_schema.name)
     message = from_json(body)
-    print(message.__dict__)
     body = message.body
     match message.message_type:
         case MessageType.RESERVATION_IMPORT_OVERLAP:
-            print("RESERVATION_IMPORT_OVERLAP: ", body)
+            LOGGER.info("%s - MessageType: RESERVATION_IMPORT_OVERLAP. Body: %s", wrapper.service_schema.name, body)
             wrapper.delete_reservation(body["old_internal_id"])
         case MessageType.RESERVATION_IMPORT_REQUEST:
-            print("RESERVATION_IMPORT_REQUEST: ", body)
+            LOGGER.info("%s - MessageType: RESERVATION_IMPORT_REQUEST.", wrapper.service_schema.name)
             for user_email, user_service in body["users_with_services"].items():
-                print(user_email, user_service)
                 if wrapper.service_schema.value in user_service:
                     reservations = wrapper.import_new_or_newly_canceled_reservations({"email": user_email})
+                    LOGGER.info("%s - Sending RESERVATION_IMPORT response. Reservations: %s", wrapper.service_schema.name, reservations)
                     channel.basic_publish(exchange=EXCHANGE_NAME, routing_key=WRAPPER_TO_CALENDAR_ROUTING_KEY,
                                           body=to_json(
                                               MessageFactory.create_import_reservations_response_message(
                                                   wrapper.service_schema, reservations)
                                           ))
         case MessageType.RESERVATION_IMPORT_CONFIRM:
+            LOGGER.info("%s - MessageType: RESERVATION_IMPORT_CONFIRM. Body: %s", wrapper.service_schema.name, body)
             print("RESERVATION_IMPORT_CONFIRM: ", body)
             wrapper.confirm_reservation(body["internal_id"])
         case MessageType.SCHEDULED_PROPERTY_IMPORT:
-            print("Scheduled import properties...")
+            LOGGER.info("%s - MessageType: SCHEDULED_PROPERTY_IMPORT.", wrapper.service_schema.name)
             for user_email, user_service in body["users_with_services"].items():
                 if wrapper.service_schema.value in user_service:
                     properties = wrapper.import_new_properties({"email": user_email})
                     if len(properties) > 0:
+                        LOGGER.info("%s - Detected new properties. Addresses: %s", wrapper.service_schema.name, [p.get("address") for p in properties])
                         print("Detected new properties --> sending it to PropertyService")
                         channel.basic_publish(exchange=EXCHANGE_NAME, routing_key=WRAPPER_TO_APP_ROUTING_KEY,
                                             body=to_json(
